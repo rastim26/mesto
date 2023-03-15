e77e2235538e317e5f991d4569c7c75dc28da425
Rastim Safarov <rast875@gmail.com>
2023-03-15 18:41:02 +0000

    fix: Submit comments

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,26 @@ let editButtonElem = document.querySelector(".profile__edit-button");
 let popupElem = document.querySelector(".popup");
 let closeButtonElem = popupElem.querySelector(".popup__close-btn");
 
+let formElem = popupElem.querySelector(".popup__container");
+let nameInput = formElem.querySelector("#name");
+let jobInput = formElem.querySelector("#job");
+
+let profileTitleElem = document.querySelector(".profile__title");
+let profileSubtitleElem = document.querySelector(".profile__subtitle");
+
 function openPopup() {
   popupElem.classList.add("popup_opened");
+
+  nameInput.value = profileTitleElem.innerText;
+  jobInput.value = profileSubtitleElem.innerText;
 }
 
 function closePopup() {
   popupElem.classList.remove("popup_opened");
 }
 
-editButtonElem.addEventListener("click", openPopup);
-closeButtonElem.addEventListener("click", closePopup);
-
-let formElem = popupElem.querySelector(".popup__container");
-let nameInput = formElem.querySelector("#name");
-let jobInput = formElem.querySelector("#job");
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  let profileTitleElem = document.querySelector(".profile__title");
-  let profileSubtitleElem = document.querySelector(".profile__subtitle");
 
   let nameValue = nameInput.value;
   let jobValue = jobInput.value;
@@ -32,4 +32,6 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+editButtonElem.addEventListener("click", openPopup);
+closeButtonElem.addEventListener("click", closePopup);
 formElem.addEventListener("submit", handleFormSubmit);
