a6e5d7cab1dbd9d54bc4171e9bb31ec9782a7dfd
Rastim Safarov <rastim.safarov@appius.com>
2023-05-23 18:33:38 +0100

    refactor: Unset webpack adaptation, Delete PopupWithForm.setInputValues mehod

--- src/scripts/utils/consts.js
+++ src/scripts/utils/consts.js
@@ -3,8 +3,10 @@ const buttonOpenPopupCard = document.querySelector(".profile__add-button");
 
 const popupProfileElem = document.querySelector(".edit-popup");
 const formPopupProfileElem = popupProfileElem.querySelector(".edit-popup__form");
+const inputNameProfileElem = formPopupProfileElem.querySelector('.popup__input_type_name');
+const inputJobProfileElem = formPopupProfileElem.querySelector('.popup__input_type_info');
 
 const popupCardElement = document.querySelector(".add-popup");
 const formPopupCardElem = popupCardElement.querySelector(".add-popup__form");
 
-export { buttonOpenPopupProfile, buttonOpenPopupCard, formPopupProfileElem, formPopupCardElem };
+export { buttonOpenPopupProfile, buttonOpenPopupCard, formPopupProfileElem, inputNameProfileElem, inputJobProfileElem, formPopupCardElem };
